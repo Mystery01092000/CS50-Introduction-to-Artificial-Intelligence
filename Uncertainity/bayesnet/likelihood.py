224893bd4af7ac6ef569b2dfa134337538ab7f1e
Pranav Tomar <pranav.tomar_cs18@gla.ac.in>
2021-02-17 20:40:28 +0530

    uncertainity/bayesnet added

--- Uncertainity/bayesnet/likelihood.py
+++ Uncertainity/bayesnet/likelihood.py
@@ -1,6 +1,6 @@
 from model import model
 
 # Calculate probability for a given observation
-probability = model.probability([["heavy", "yes", "delayed", "miss"]])
+probability = model.probability([["heavy", "yes", "delayed", "miss"],["none", "no", "on time", "attend"]])
 
 print(probability)
